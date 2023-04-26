2b271f15f88cd1e707840757b73ba2efb8084ae6
Adilson Lima de Paula <adilsonmicuim@gmail.com>
2023-04-25 22:51:36 -0300

    criando as tabelas no banco de dados

--- product/models/category.py
+++ product/models/category.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(_("Title"), max_length=100)
-    slug = models.SlugField(_("Slug"), unique=True)
-    description = models.CharField(_("Description"), max_length=200, blank=True, null=True)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
     active = models.BooleanField(default=True)
 
     def __unicode__(self):
--- product/models/product.py
+++ product/models/product.py
@@ -4,8 +4,8 @@ from product.models.category import Category
 
 
 class Product(models.Model):
-    title = models.models.CharField(_("Title"), max_length=100)
-    description = models.TextField(_("Description"), max_length=500, blank=True, null=False)
-    price = models.PositiveIntegerField(_("Price"), null=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True, null=False)
+    price = models.PositiveIntegerField(null=True)
     active = models.BooleanField(default=True)
-    categories = models.ManyToMany(Category, blank=True)
+    category = models.ManyToManyField(Category, blank=False)
